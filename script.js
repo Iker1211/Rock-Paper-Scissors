b37e83545d68e1979d501ece84dd88425590f51d
iker1211 <velezlucasiker1@gmail.com>
2025-01-06 08:27:45 -0500

    add UI with the DOM

--- script.js
+++ script.js
@@ -1,76 +1,88 @@
 function getComputerChoice (){
-    const options = ["rock", "paper", "scissors"]; 
+   const options = ["rock", "paper", "scissors"]; 
 
-    let choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
-}
-
-function getHumanChoice () {
-    let ask = prompt("Whats your option? ");
-    return ask.toLowerCase();
+   let choice = options[Math.floor(Math.random() * options.length)];
+   return choice;
 }
 
 //const rounds = 5
 
 function playGame () {
 
-   /* Here Im importing the results of computerchoice function */
-   let computerSelection = getComputerChoice(); 
+  /* Here Im importing the results of computerchoice function */
 
-    let humanScore = 0;
-    let computerScore = 0;
+   let humanScore = 0;
+   let computerScore = 0;
+  // How to tab a block of code in VSCode?
+   //Volver a usar playRound, hacer toda la monda dentro y hacer returns
 
-    //Volver a usar playRound, hacer toda la monda dentro y hacer returns
+   function playRound (humanChoice) {
 
-    function playRound (humanChoice, computerChoice) {
-     }
+     let humanPoints = document.getElementById("human-score");
+     let computerPoints = document.getElementById("computer-score");
 
-     document.addEventListener('DOMContentLoaded', function(event) {
-      const gameZone = document.getElementById("game"); 
-         const humanPoints = document.getElementById("human-score");
-         const computerPoints = document.getElementById("computer-score");
+     let computerChoice = getComputerChoice(); 
 
-        gameZone.addEventListener('click', (event) => {
-        let target = event.target;
-     
-           switch(target.id) {
-              case 'btn-rock':
-                 playRound("rock", computerSelection);
-                  if ( computerSelection === "paper" ) {
-                     computerScore ++;
-                     console.log('computer won!');
-                  } else if ( computerSelection === "rock" ) {
-                     computerScore ++;
-                     humanScore ++;
-                     console.log('empate');
-                  } else {
-                     humanScore ++;
-                     console.log('human won!');
-                  }
-                 break;
-              case 'btn-paper':
-                 playRound("paper", computerSelection);
-
-                 break;
-              case 'btn-scissors':
-                 playRound("scissors", computerSelection);
-
-                 break;
-           }
-        });
-     });
-
-   //   for ( x = 0; x < times; x ++){
-   //    playRound (getHumanChoice(), computerSelection);
-   //  }
- 
-    if (humanScore > computerScore ) {
-      alert('Human won!')
-    } else if (computerScore > humanScore) {
-      alert('Computer won!')
+     if (humanChoice === computerChoice) {
+        console.log(humanScore);
+        console.log(computerScore);
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+        humanPoints.textContent = humanScore;
     } else {
-      alert('empate mi so')
+        computerScore++;
+        console.log(humanScore);
+        console.log(computerScore);
+        computerPoints.textContent = computerScore;
     }
- }
+
+      if (humanScore === 5) {
+
+        humanPoints.textContent = humanScore;
+        computerPoints.textContent = computerScore;
+        alert("You won the game!");
+        humanScore = 0;
+        computerScore = 0;
+        humanPoints.textContent = humanScore;
+        computerPoints.textContent = computerScore;
+      } else if (computerScore === 5) {
+
+        humanPoints.textContent = humanScore;
+        computerPoints.textContent = computerScore;
+        alert("You lost the game!");
+        humanScore = 0;
+        computerScore = 0;
+        humanPoints.textContent = humanScore;
+        computerPoints.textContent = computerScore;
+     }
+    }
+
+    document.addEventListener('DOMContentLoaded', function(event) {
+     const gameZone = document.getElementById("game"); 
+
+       gameZone.addEventListener('click', (event) => {
+       let target = event.target;
+       let result;
+
+          switch(target.id) {
+             case 'btn-rock':
+                 result = playRound("rock");
+                break;
+             case 'btn-paper':
+                 result = playRound("paper");
+                break;
+             case 'btn-scissors':
+                 result = playRound("scissors");
+                break;
+          }
+       });
+    });
+}
 
 playGame ();
