d03f4c08e8c89e27ebc22cd7cc5175c7afc1aab3
iker1211 <velezlucasiker1@gmail.com>
2025-01-05 21:15:00 -0500

    Improve for UI experience

--- script.js
+++ script.js
@@ -10,46 +10,67 @@ function getHumanChoice () {
     return ask.toLowerCase();
 }
 
-const rounds = 5
+//const rounds = 5
 
-function playGame (times) {
+function playGame () {
 
-   const computerSelection = getComputerChoice();
+   /* Here Im importing the results of computerchoice function */
+   let computerSelection = getComputerChoice(); 
 
     let humanScore = 0;
     let computerScore = 0;
- 
-    for ( x = 0; x < times; x ++){
-      playRound (getHumanChoice(), computerSelection);
-    }
 
-    if (humanScore > computerScore) {
+    //Volver a usar playRound, hacer toda la monda dentro y hacer returns
+
+    function playRound (humanChoice, computerChoice) {
+     }
+
+     document.addEventListener('DOMContentLoaded', function(event) {
+      const gameZone = document.getElementById("game"); 
+         const humanPoints = document.getElementById("human-score");
+         const computerPoints = document.getElementById("computer-score");
+
+        gameZone.addEventListener('click', (event) => {
+        let target = event.target;
+     
+           switch(target.id) {
+              case 'btn-rock':
+                 playRound("rock", computerSelection);
+                  if ( computerSelection === "paper" ) {
+                     computerScore ++;
+                     console.log('computer won!');
+                  } else if ( computerSelection === "rock" ) {
+                     computerScore ++;
+                     humanScore ++;
+                     console.log('empate');
+                  } else {
+                     humanScore ++;
+                     console.log('human won!');
+                  }
+                 break;
+              case 'btn-paper':
+                 playRound("paper", computerSelection);
+
+                 break;
+              case 'btn-scissors':
+                 playRound("scissors", computerSelection);
+
+                 break;
+           }
+        });
+     });
+
+   //   for ( x = 0; x < times; x ++){
+   //    playRound (getHumanChoice(), computerSelection);
+   //  }
+ 
+    if (humanScore > computerScore ) {
       alert('Human won!')
     } else if (computerScore > humanScore) {
       alert('Computer won!')
     } else {
       alert('empate mi so')
     }
-
-    function playRound (humanChoice, computerChoice) {
-      if (humanChoice == computerChoice) {
-         computerScore ++;
-         humanScore ++;
-         console.log('empate mi so');
-      } else if (humanChoice == "rock" && computerChoice == "paper") {
-         computerScore++; 
-         console.log('computer won!');
-      } else if (humanChoice == "paper" && computerChoice == "scissors"){
-         computerScore++;
-         console.log('computer won!');
-      } else if (humanChoice == "scissors" && computerChoice == "rock") {
-         computerScore++;
-         console.log('computer won!');
-      } else {
-         humanScore++;
-         console.log('human won!');
-      }
-     }
  }
 
-playGame (rounds);
+playGame ();
